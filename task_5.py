6c6a218177b2c1ad139e9f4bf0e34b2b32d1d5d4
Adalberto777 <albert.tliashev@gmail.com>
2022-10-19 00:06:12 +0500

    end191022

--- task_5.py
+++ task_5.py
@@ -2,12 +2,30 @@
 # Пример:
 # - для k = 8 список будет выглядеть так: [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 
-n = int(input())
-if n == 0:
-    print(0)
-else:
-    a, b = 0, 1
-    for i in range(2, n + 1):
-        a, b = b, a + b
+def nega_fibo(n):
+    str = []
+    str_temp = []
+    str_nega_fibo = []
+    if n == 0:
+        str_nega_fibo = [0]        
+    else:
+        str = [0, 1]
+        str_temp = [0, 1]
+        a, b, j = 0, 1, - 1
         
-    print(b)
+        for i in range(2, n + 1):
+            str.append(a + b)
+            str_temp.append((a + b) * j) 
+            a, b = b, a + b
+            j *= -1              
+        
+        for i in range(0, n):
+            str_nega_fibo.append(str_temp[n - i])
+            
+        str_nega_fibo = str_nega_fibo + str
+    return str_nega_fibo
+
+
+n = int(input('Enter N: '))
+    
+print(nega_fibo(n))
