cdceca808b7479e943e282e6ddab927b88fbc63f
Adalberto777 <albert.tliashev@gmail.com>
2022-10-21 00:06:07 +0500

    task_3_1

--- task_1_1.py
+++ task_1_1.py
@@ -5,3 +5,12 @@
 users_nums = [2, 3, 5, 9, 3]
 
 print(f'Sum => {sum(users_nums[1::2])}')
+
+# from random import randint
+
+# my_list = [randint(10, 100) for i in range(randint(5, 10))]
+# print(my_list)
+# sum_digit = 0
+
+# for el in range(1, len(my_list), 2):
+# sum_digit += my_list[el]
