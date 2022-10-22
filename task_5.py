d1479419033750cb3da53e42913af00f6d68815a
Adalberto777 <albert.tliashev@gmail.com>
2022-10-23 03:18:52 +0500

    task_5+

--- task_5.py
+++ task_5.py
@@ -4,24 +4,28 @@
 
 def nega_fibo(n):
     str = []
-    str_temp = []
+    # str_temp = []
     str_nega_fibo = []
     if n == 0:
         str_nega_fibo = [0]        
     else:
         str = [0, 1]
-        str_temp = [0, 1]
+        # str_temp = [0, 1]
+        str_nega_fibo = [0, 1]
         a, b, j = 0, 1, - 1
         
         for i in range(2, n + 1):
             str.append(a + b)
-            str_temp.append((a + b) * j) 
+            # str_temp.append((a + b) * j) 
+            str_nega_fibo.append((a + b) * j) 
             a, b = b, a + b
             j *= -1              
         
-        for i in range(0, n):
-            str_nega_fibo.append(str_temp[n - i])
-            
+        # for i in range(0, n):
+        #     str_nega_fibo.append(str_temp[n - i])
+
+        str_nega_fibo.pop(0)
+        str_nega_fibo.reverse()        
         str_nega_fibo = str_nega_fibo + str
     return str_nega_fibo
 
